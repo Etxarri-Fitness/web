b9afb6598d6323893527edcc21b3425af0660515
Ibarra <aitoribarra@tutanota.com>
2022-06-11 19:32:29 +0200

    UPDATE - Global

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from "next";
 import { Fragment } from "react";
 
 import HomePage from "/components/layout/HomePage";
+import Pricing from "/components/layout/Pricing";
 
 const Home: NextPage = () => {
   return (
     <Fragment>
       <HomePage />
+      <Pricing />
     </Fragment>
   );
 };
